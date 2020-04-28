3f279727d1837d32519b875d5ed0acde41a3c5e7
Jalaz Kumar <jalaz.kumar@indiamart.com>
2020-04-29 01:54:52 +0530

    Added token-based Auth

--- service/config.py
+++ service/config.py
@@ -1,7 +1,9 @@
+from os import environ
+
 class Config(object):
     DEBUG = True
-    SECRET_KEY = 'the quick brown fox jumps over the lazy dog'
+    SECRET_KEY = environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///auth.db'
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
