e0fc786cdf124409137f8c52bca970f0adf6c70c
Jalaz Kumar <jalaz.kumar@indiamart.com>
2020-05-02 02:39:37 +0530

    Added Postgres as production DB

--- service/config.py
+++ service/config.py
@@ -1,9 +1,17 @@
 from os import environ
+from api.log import logger
 
-class Config(object):
+class DevelopmentConfig(object):
     DEBUG = True
     SECRET_KEY = environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = environ.get("DEVELOPMENT_DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+class ProductionConfig(object):
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = environ.get("PRODUCTION_DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
