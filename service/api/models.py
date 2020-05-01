1db0236ee93c8975268fb684f8d526af9132e98a
Jalaz Kumar <jalazkumar1208@gmail.com>
2020-05-02 03:38:39 +0530

    Deployment issue fix: 8

--- service/api/models.py
+++ service/api/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
-    password_hash = db.Column(db.String(64))
+    password_hash = db.Column(db.String(512))
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
