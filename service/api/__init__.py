df75df059e70b0729e5180f0b54f3b7bd35fdfda
Jalaz Kumar <jalazkumar1208@gmail.com>
2020-05-02 02:39:37 +0530

    Added Postgres as production DB

--- service/api/__init__.py
+++ service/api/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
+from .log import logger
 
 db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+
+    if environ.get('ENV') == 'PRODUCTION':
+        app.config.from_object('config.ProductionConfig')
+        logger.info("Loaded: Configuration of Production")
+    else:
+        app.config.from_object('config.DevelopmentConfig')
+        logger.info("Loaded: configuration of Development")
 
     db.init_app(app)
 
